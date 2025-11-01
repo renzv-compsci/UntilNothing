4dad2c3882aa3796a838811367a85c3b9b019f31
renzv-compsci <viloriarenz917@gmail.com>
2025-11-01 20:37:32 +0800

    Create boiler plates for linking of game script and engine

---
+++ engine/src/engine_lua.c
@@ -0,0 +1,23 @@
+#include "engine_lua.h"
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#include <stdio.h>
+
+static lua_State *L = NULL;
+
+void engine_lua_init(const char* game_script_path) {
+
+}
+
+void engine_lua_update(void) {
+
+}
+
+void engine_lua_draw(void) {
+
+}
+
+void engine_lua_shutdown(void) {
+
+}
